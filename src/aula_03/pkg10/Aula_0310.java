a5868d1552417e6f3c62f86ca99fba9637b9d16e
feliperaymison <43821828+feliperaymison@users.noreply.github.com>
2018-10-03 12:39:46 -0400

    Update Aula_0310.java

--- src/aula_03/pkg10/Aula_0310.java
+++ src/aula_03/pkg10/Aula_0310.java
@@ -5,6 +5,7 @@ public class Aula_0310 {
     public static void main(String[] args) {
         System.out.println("Today is 03/10/2018");
         System.out.println("Sending from git");
+        System.out.println("ALEXANDRE VAGABUNDO");
     }
     
 }
